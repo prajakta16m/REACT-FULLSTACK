336c90a885043dd7c3e70da519933ab00f1827f5
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2023-10-08 17:11:03 +0530

    updated comments section

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -8,13 +8,19 @@ function Post() {
     let { id } = useParams();
 
     const [ postObj, setPostObj ] = useState({});
+    const [ comments, setComments ] = useState([]);
     
     useEffect(() => {
         axios.get(`http://localhost:3001/posts/byId/${id}`).then((resp) => {
-            console.log(resp.data);
+           
             setPostObj(resp.data);
         });
-    });
+
+        axios.get(`http://localhost:3001/comments/${id}`).then((resp) => {
+           
+           setComments(resp.data);
+        });
+    }, []);
 
   return (
     <div className='postPage'>
@@ -26,7 +32,19 @@ function Post() {
             </div>
             
         </div>
-        <div className='rightSide'>Comments Section</div>
+        <div className='rightSide'>
+            <div className='addCommentContainer'>
+                <input type='text' placeholder='Comment...' autoComplete='off' />
+                <button>Add comment</button>
+            </div>
+            <div className='listOfComments'>
+                {comments.map((comment, key) => {
+                    return <div className='comment'>
+                        {comment.commentBody}
+                    </div>;
+                })}
+            </div>
+        </div>
     </div>
   )
 }
