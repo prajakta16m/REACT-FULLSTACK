91ac87d86013f256345f4e096e7d374d54fc1b6a
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2023-10-17 00:31:25 +0530

    jsonwebtoken to generate accesstoken

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
+const { sign } = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
@@ -24,7 +25,10 @@ router.post("/login", async (req, res) => {
   bcrypt.compare(password, user.password).then(async (match) => {
     if (!match) res.json({ error: "Wrong Username And Password Combination" });
 
-    res.json("YOU LOGGED IN!!!");
+    // Generate jsonwebtoken or accesstoken
+    const accesstoken = sign({username: user.username, id: user.id}, "importantsecret");
+
+    res.json(accesstoken);
   });
 });
 
