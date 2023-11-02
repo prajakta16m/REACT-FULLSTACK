d78379a3d6f01b33a875fe0d5339c6a918884b4a
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2023-11-02 15:27:43 +0530

    Liking System - Likes Table

--- server/models/Users.js
+++ server/models/Users.js
@@ -12,7 +12,12 @@ module.exports = (sequelize, DataTypes) => {
         }
     });
 
-   
+    Users.associate = (models) => {
+        Users.hasMany(models.Likes, {
+          onDelete: "cascade",
+        });
+  
+      };
 
     return Users;
 }
