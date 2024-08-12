7bd4c84d67dcdac269c6452f5d45f8bec2d06f11
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2024-08-12 18:31:44 +0530

    use basic fetch and list data

--- problems/fetchFromAPI.js
+++ problems/fetchFromAPI.js
@@ -1,1 +1,26 @@
+import {useState, useEffect} from 'react';
+
+export default function App() {
+
+    const userData = [];
+    const [data, setData] = useState([{name: 'Default'}]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => {
+                return res.json();
+            }).then ( (json) => {
+                console.log(json);
+                setData(json);
+            });
+    }, []);
+
+  return <>
+      <h1>Hello world</h1>
+  
+      <div>
+          { data?.map( (obj) => <div>{obj.name}</div> ) }
+      </div>
+  </>
+}
 
